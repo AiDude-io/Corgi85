73f497ae66d80f9065d27629e96affe289f78a9e
Nat <nat.wrw@gmail.com>
2020-07-08 13:51:16 +0700

    add http request

--- src/Corgi85/modules/CorgiIFTTT.hpp
+++ src/Corgi85/modules/CorgiIFTTT.hpp
@@ -3,6 +3,9 @@
 #define CORGI_IFTT_MODULE_H
 
 #include "CorgiModule.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <StringSplitter.h>
 
 
 class CorgiIFTTT : public CorgiModule {
@@ -12,8 +15,37 @@ class CorgiIFTTT : public CorgiModule {
     };
 
     void cmd(String cmd) {
-      Serial.printf("MODULE = %s\r\nCMD=%s\r\n", this->name(), cmd.c_str() );
-    };
+      StringSplitter *splitter = new StringSplitter(cmd, ',', 6); 
+      if (splitter->getItemCount() >= 2) {
+
+        String func = String(splitter->getItemAtIndex(1));
+        String v1 = String(splitter->getItemAtIndex(2));
+        String v2 = String(splitter->getItemAtIndex(3));
+        String v3 = String(splitter->getItemAtIndex(4));
+        String v4 = String(splitter->getItemAtIndex(5));      
+        if (func == "fire") {
+          Serial.println("fire!");
+          Serial.println(v1);
+          HTTPClient http; 
+          http.begin(v1); //Specify the URL
+          int httpCode = http.GET();
+      
+          if (httpCode > 0) { //Check for the returning code
+      
+              String payload = http.getString();
+              Serial.println(httpCode);
+              Serial.println(payload);
+            }
+      
+          else {
+            Serial.println("Error on HTTP request");
+          }
+      
+          http.end(); //Free the resources          
+        }
+      };
+      
+    }
 
 
     const char* name() {
